727e84011ea37f8a8767cafc392254e7541a997e
avest <avest1000@gmail.com>
2014-03-29 14:40:21 +0800

    修改类名。

--- src/SentenceExtraction/SentenceDetector.java
+++ src/SentenceExtraction/SentenceDetector.java
@@ -5,20 +5,20 @@ import java.util.List;
 import edu.stanford.nlp.process.DocumentPreprocessor;
 
 /**
- * Stanford的断句器
- * 输入文本路径
- * 输出断句后的String数组
+ * Stanford断句器
  * @author qiusd
  */
-public class StanfordSentenceDetector {
-
+public class SentenceDetector {
+	/**
+	 * 输入文本路径
+	 * 输出断句后的String数组
+	 */
 	public static String[] split(String docPath) {
 		DocumentPreprocessor dp = new DocumentPreprocessor(docPath);
 		ArrayList<String> list = new ArrayList<String>();
 		for (List sentence : dp) {
 			StringBuilder sb = new StringBuilder();
-			for(Object word : sentence)
-			{
+			for (Object word : sentence) {
 				sb.append(word.toString() + " ");
 			}
 			list.add(sb.toString());
@@ -26,9 +26,8 @@ public class StanfordSentenceDetector {
 		String[] strArray = new String[list.size()];
 		list.toArray(strArray);
 		System.out.println("The sentences:");
-		for(int i = 0; i < list.size(); i++)
-		{
-			System.out.println((i+1) + ". " + strArray[i]);
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println((i + 1) + ". " + strArray[i]);
 		}
 		return strArray;
 	}
