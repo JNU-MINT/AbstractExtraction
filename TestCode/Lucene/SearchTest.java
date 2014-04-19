4c38f8eff3cb08794c8654a52dd88a0af9bede3f
avest <avest1000@gmail.com>
2014-04-19 15:00:19 +0800

    修改函数调用方式

--- TestCode/Lucene/SearchTest.java
+++ TestCode/Lucene/SearchTest.java
@@ -6,10 +6,10 @@ public class SearchTest {
 
 	public static void main(String[] args) throws Exception {
 		IndexSearchProcess st = new IndexSearchProcess(
-				"C:\\Users\\qiusd\\Desktop\\index");
+				"F:\\patent(F)\\index");
 //		String[] fields = {"last-name"};
 //		st.searchInMultiFields(fields, "Wesinger, Jr. Ralph E.");
-//		st.search("kind", "P2");
-		st.searchInAllFields("Pleasant Garden");
+		//st.search("date", "20111227");
+		System.out.println(st.searchInAllFields("guangzhou", Integer.MAX_VALUE));
 	}
 }
