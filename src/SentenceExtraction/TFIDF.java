c10c4f87e9faac276c2177f7fecb762605460f9f
avest <avest1000@gmail.com>
2014-04-26 01:27:51 +0800

    重构代码，并增加利用Lucene搜索引擎计算IDF的函数。

--- src/SentenceExtraction/TFIDF.java
+++ src/SentenceExtraction/TFIDF.java
@@ -1,41 +1,37 @@
 package SentenceExtraction;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import edu.fudan.nlp.cn.tag.CWSTagger;
-import edu.fudan.util.exception.LoadModelException;
+import Lucene.SearchProcessor;
 
+import processor.FileProcessor;
+import processor.SentenceProcessor;
+
+/**
+ * 仅考虑有意义的词
+ * 
+ * @author qiusd
+ */
 public class TFIDF {
-	private CWSTagger cWSTagger;
-	private static List<String> fileList = new ArrayList<String>();
-	private static HashMap<String,Double> tf = new HashMap<String,Double>();
-	private static HashMap<String,Double> tfidf = new HashMap<String,Double>();
-	//private static HashMap<String, HashMap<String, Float>> allTheTf = new HashMap<String, HashMap<String, Float>>();
-	//private static HashMap<String, HashMap<String, Integer>> allTheNormalTF = new HashMap<String, HashMap<String, Integer>>();
 
-	/*
-	 * 
-	 */
-	public TFIDF() throws LoadModelException {
-		cWSTagger = new CWSTagger("models/seg.m");
+	private HashMap<String, Double> tfMap = new HashMap<String, Double>();
+	private HashMap<String, Double> idfMap = new HashMap<String, Double>();
+	private HashMap<String, Double> tfidfMap = new HashMap<String, Double>();
+	private SentenceProcessor sentenceProcess;
+
+	public TFIDF() throws Exception {
+		sentenceProcess = new SentenceProcessor();
 	}
 
-	// 找出text中有意义的词，返回一个HashMap
-	public static void getMeaningfulWords(String text)throws Exception {
-		SentenceProcess getWords = new SentenceProcess();
-		String[] meaningfulWords = getWords.getMeaningfulWords(text);
+	/**
+	 * 计算TF
+	 */
+	private void calTf(String text) throws Exception {
+		String[] meaningfulWords = sentenceProcess.getStemmedWord(text);
 		int wordNum = meaningfulWords.length;
-		//HashMap<String, Double> tfidfWords = new HashMap<String, Double>();// 正规化
 		int wordtf;
 		for (int i = 0; i < wordNum; i++) {
 			wordtf = 0;
@@ -48,140 +44,104 @@ public class TFIDF {
 				}
 			}
 			if (meaningfulWords[i] != " ") {
-				tf.put(meaningfulWords[i], (new Double(++wordtf)) / wordNum);
+				tfMap.put(meaningfulWords[i], (new Double(++wordtf)) / wordNum);
 				meaningfulWords[i] = " ";
 			}
-			// System.out.println(meaningfulWords[i]);
 		}
 	}
 
-	//输入一个文件库的目录，遍历文件并计算一个text的tfidf值
-	public static void getTFIDF(String text,String dir) throws Exception {  
-      //公式IDF＝log((1+|D|)/|Dt|)，其中|D|表示文档总数，|Dt|表示包含关键词t的文档数量。  	
-		getMeaningfulWords(text);
-		readDirs(dir);
-	          float D = fileList.size();//文档总数  
-	          int count;
-	          double idf;
-	          int n=0,i;
-	          ArrayList<String[]> allWord = new ArrayList<String []>();
-	          for(i = 0; i < D; i++){
-	        	  //System.out.println(i);
-	        	  String[] meaningfulWords1 = cutWord(fileList.get(i));
-	        	  allWord.add(meaningfulWords1);
-	          }
-	          for(String tfidfWord : tf.keySet()) {
-	        	  count = 0;
-	          for(i=0; i<D; i++){
-				//String[] meaningfulWords1 = cutWord(fileList.get(i));
-				//n++;
-				//System.out.println(n);
-	        	  for(String word:allWord.get(i)){
-	        		  if (word.equals(tfidfWord)){
-	        			  count++;
-	        			  break;
-	        		  }
-	        	  }
-	          }
-	          idf = TFIDF.log(D, count);
-	          System.out.println(tf.get(tfidfWord)+" "+idf);//输出每个词的tf,idf
-	          tfidf.put(tfidfWord, tf.get(tfidfWord)/idf);
-	          }
-	      }  
-
-	public static float log(float value, float base) {
-		return (float) (Math.log(value) / Math.log(base));
-	}
-
-	public static void readDirs(String filepath)throws FileNotFoundException, IOException {
-		try {
-			File file = new File(filepath);
-			if (!file.isDirectory()) {
-				System.out.println("输入的参数应该为[文件夹名]");
-				System.out.println("filepath: " + file.getAbsolutePath());
-			} else if (file.isDirectory()) {
-				String[] filelist = file.list();
-				for (int i = 0; i < filelist.length; i++) {
-					File readfile = new File(filepath + "\\" + filelist[i]);
-					if (!readfile.isDirectory()) {
-						// System.out.println("filepath: " +
-						// readfile.getAbsolutePath());
-						fileList.add(readfile.getAbsolutePath());
-					} else if (readfile.isDirectory()) {
-						readDirs(filepath + "\\" + filelist[i]);
+	/**
+	 * 利用语料库计算IDF
+	 */
+	private void calIdf(String text, String dir) throws Exception {
+		// IDF＝log((1+|D|)/|Dt + 1|)
+		// |D|表示文档总数，|Dt|表示包含关键词t的文档数量
+		FileProcessor fileProcessor = new FileProcessor();
+		List<String> fileList = fileProcessor.getFilesByDir(dir);
+		float D = fileList.size();
+		int count;
+		ArrayList<String[]> corpusList = new ArrayList<String[]>();
+		for (int i = 0; i < D; i++) {
+			String corpusString = fileProcessor.readFiles(fileList.get(i));
+			String[] meaningfulWords = sentenceProcess
+					.getMeaningfulWords(corpusString);
+			corpusList.add(meaningfulWords);
+		}
+		for (String meaningfulWord : tfMap.keySet()) {
+			count = 0;
+			for (String[] corpusWords : corpusList) {
+				for (String corpusWord : corpusWords) {
+					if (corpusWord.equals(meaningfulWord)) {
+						count++;
+						break;
 					}
 				}
 			}
-
-		} catch (FileNotFoundException e) {
-			System.out.println(e.getMessage());
+			double idf = Math.log(D / (count + 1));
+			idfMap.put(meaningfulWord, idf);
 		}
-		//return fileList;
 	}
 
-	public static String readFiles(String file) throws FileNotFoundException,
-			IOException {
-		StringBuffer sb = new StringBuffer();
-		InputStreamReader is = new InputStreamReader(new FileInputStream(file),
-				"gbk");
-		BufferedReader br = new BufferedReader(is);
-		String line = br.readLine();
-		while (line != null) {
-			sb.append(line).append("\r\n");
-			line = br.readLine();
+	/**
+	 * 利用Lucene计算IDF
+	 */
+	private void calIdf(String text) throws Exception {
+		float D = 1265667;
+		SearchProcessor st = new SearchProcessor("F:\\patent(F)\\index");
+		for (String meaningfulWord : tfMap.keySet()) {
+			int count = st.getAllFieldsSearchNum(meaningfulWord);
+			double idf = Math.log(D / (count + 1));
+			idfMap.put(meaningfulWord, idf);
 		}
-		br.close();
-		return sb.toString();
+		st.closeAll();
 	}
 
-	public static String[] cutWord(String file) throws Exception {
-		String text = readFiles(file);	
-		SentenceProcess getWords = new SentenceProcess();
-		String[] cutWordResult = getWords.getMeaningfulWords(text);
-		return cutWordResult;
+	/**
+	 * 利用语料库计算TFIDF
+	 */
+	public Map<String, Double> calTfidf(String text, String dir) throws Exception {
+		calTf(text);
+		calIdf(text, dir);
+		for (String tfidfWord : tfMap.keySet()) {
+			tfidfMap.put(tfidfWord,
+					tfMap.get(tfidfWord) * idfMap.get(tfidfWord));
+		}
+		return tfidfMap;
 	}
 
-/*	public static HashMap<String, Float> tf(String[] cutWordResult) {
-		HashMap<String, Float> tf = new HashMap<String, Float>();// 正规化
-		int wordNum = cutWordResult.length;
-		int wordtf = 0;
-		for (int i = 0; i < wordNum; i++) {
-			wordtf = 0;
-			for (int j = 0; j < wordNum; j++) {
-				if (cutWordResult[i] != " " && i != j) {
-					if (cutWordResult[i].equals(cutWordResult[j])) {
-						cutWordResult[j] = " ";
-						wordtf++;
-					}
-				}
-			}
-			if (cutWordResult[i] != " ") {
-				tf.put(cutWordResult[i], (new Float(++wordtf)) / wordNum);
-				cutWordResult[i] = " ";
-			}
+	/**
+	 * 利用Lucene计算TFIDF
+	 */
+	public Map<String, Double> calTfidf(String text) throws Exception {
+		calTf(text);
+		calIdf(text);
+		for (String tfidfWord : tfMap.keySet()) {
+			tfidfMap.put(tfidfWord,
+					tfMap.get(tfidfWord) * idfMap.get(tfidfWord));
 		}
-		return tf;
-	}
-*/
-	public void test(String s) {
-		String[] words = cWSTagger.tag2Array(s);
-		for (int i = 0; i < words.length; i++) {
-			System.out.println(words[i]);
-		}
+		return tfidfMap;
 	}
 
 	public static void main(String args[]) throws Exception {
 		String s = "A method for bulk transport, the method comprising:"
-				+ "positioning an inflatable bladder over a gravity-fed closable aperture in a bulk shipping container; and"
-				+ "after inflation of said bladder, introducing a bulk material into said container, whereby said bladder at least substantially impedes access by said bulk material to said aperture, said bladder remaining inflated during loading of said container;"
-				+ "said bladder having an upper bound and a lower bound, said method including positioning said lower bound proximal said aperture and introducing said bulk material into said rail car at a height not exceeding the height of said upper bound.";
-		// TFIDF test1 = new TFIDF();
-		// test1.test(s);
-		// TFIDF.getTFIDF(s, true);
-		//HashMap<String, Double> h = TFIDF.getMeaningfulWords(s);
-		//TFIDF.getMeaningfulWords(s);
-		TFIDF.getTFIDF(s,"C:\\work\\AbstractExtract\\task140410\\expeMaterial\\xmlFiles");
-		for (String hh : tfidf.keySet())
-			System.out.println(hh + " " + tfidf.get(hh));
+				+ "positioning an inflatable bladder over a gravity-fed "
+				+ "closable aperture in a bulk shipping container; and"
+				+ "after inflation of said bladder, introducing a bulk"
+				+ " material into said container, whereby said bladder at "
+				+ "least substantially impedes access by said bulk material"
+				+ " to said aperture, said bladder remaining inflated during "
+				+ "loading of said container;said bladder having an upper "
+				+ "bound and a lower bound, said method including positioning "
+				+ "said lower bound proximal said aperture and introducing "
+				+ "said bulk material into said rail car at a height not "
+				+ "exceeding the height of said upper bound.";
+
+		TFIDF tfidf = new TFIDF();
+		// tfidf.calTfidf(s,
+		// "F:\\百度云\\研究方向\\一种基于背景知识的摘要提取算法\\task\\task140410\\实验材料\\文件库");
+		Map<String, Double> map = tfidf.calTfidf(s);
+		for (String hh : map.keySet())
+			System.out.println(hh + ": " + map.get(hh) + ", "
+					+ map.get(hh) + ", " + map.get(hh));
 	}
 }
